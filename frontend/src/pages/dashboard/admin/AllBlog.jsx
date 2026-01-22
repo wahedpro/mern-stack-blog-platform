a63b56dd22ad1e09fc6229573f1bd3ddc0af24c2
wahedpro <wahedulislam.pro@gmail.com>
2026-01-22 15:15:49 +0600

    get all the blog and update status

--- frontend/src/pages/dashboard/admin/AllBlog.jsx
+++ frontend/src/pages/dashboard/admin/AllBlog.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import apiRequestHandler from "../../../services/ApiRequestHandler";
+import { RxUpdate } from "react-icons/rx";
 
 const AllBlog = () => {
   const [allPosts, setAllPosts] = useState([]);
@@ -18,14 +19,45 @@ const AllBlog = () => {
     getAllPost();
   }, []);
 
+  const publishPost = async (postId) => {
+    try {
+      const token = localStorage.getItem("token");
+      const result = await apiRequestHandler(
+        `posts/${postId}/publish`,
+        "PATCH",
+        null,
+        token,
+      );
+      console.log("Post published:", result);
+    } catch (error) {
+      console.error("Failed to publish post", error);
+    }
+  };
+
   return (
     <div className="grid gap-6 px-6 pt-6 sm:grid-cols-2 lg:grid-cols-5">
       {allPosts.map((post) => (
         <div key={post._id} className="bg-white shadow-sm">
-          <img src={post.thumbnail} className="h-44 w-full object-cover rounded-t-xl"/>
-          <div className="p-4">
-            <h3 className="text-sm font-semibold">{post.title}</h3>
-            <p className="text-red-400">{post.status}</p>
+          <img
+            src={post.thumbnail}
+            className="h-44 w-full object-cover rounded-t-xl"
+          />
+          <div className="flex justify-between items-center">
+            <div className="p-4">
+              <h3 className="text-sm font-semibold">{post.title}</h3>
+              <p className="text-red-400">{post.status}</p>
+            </div>
+            <button
+              disabled={post.status === "PUBLISHED"}
+              className={`p-2 rounded text-xs mr-2 ${
+                post.status === "PUBLISHED"
+                  ? "bg-gray-200 cursor-not-allowed"
+                  : "bg-green-600 hover:bg-green-700 text-white"
+              }`}
+              onClick={() => publishPost(post._id)}
+            >
+              <RxUpdate size={20} />
+            </button>
           </div>
         </div>
       ))}
--- frontend/src/pages/dashboard/admin/AllUser.jsx
+++ frontend/src/pages/dashboard/admin/AllUser.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import apiRequestHandler from "../../../services/ApiRequestHandler";
+import { FaUser } from "react-icons/fa";
 
 const AllUser = () => {
-  return (
-    <div>AllUser</div>
-  )
-}
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getAllUser = async () => {
+      const token = localStorage.getItem("token");
+      const result = await apiRequestHandler("users", "GET", null, token);
+      setUsers(result.data);
+    };
+    getAllUser();
+  }, []);
 
-export default AllUser
+  return (
+    <div className="text-sm rounded grid grid-cols-5 gap-5">
+      {users.map((user) => (
+        <div
+          key={user._id}
+          className="flex flex-col items-center justify-between py-8 border border-gray-200"
+        >
+          {user.profilePicture === "" ? (
+            <FaUser className="bg-gray-100 h-24 w-24 p-2 rounded-full object-cover" />
+          ) : (
+            <img
+              className="h-24 w-24 rounded-full object-cover"
+              src={user.profilePicture}
+              alt={user.name}
+            />
+          )}
+
+          <h2 className="text-lg text-gray-800 mt-3">{user.name}</h2>
+
+          <p className="text-xs text-gray-500">{user.email}</p>
+
+          <p
+            className={`px-2 py-0.5 rounded-full mt-2 text-xs border ${
+              user.role === "admin"
+                ? "bg-green-500/20 text-green-600 border-green-500/30"
+                : "bg-blue-500/20 text-blue-600 border-blue-500/30"
+            }`}
+          >
+            {user.role}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AllUser;
